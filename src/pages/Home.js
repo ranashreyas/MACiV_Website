5cb6098a43a305e079b2d2ceb1dd5ae4c2b9b6ab
ranashreyas <ranashreyas@gmail.com>
2025-06-22 19:08:00 -0700

    more

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h1>Welcome to the Home Page</h1>
-      <p>This is a placeholder for the home page content!!!</p>
+      <p>This is a placeholder for the home page content!!!!!</p>
     </div>
   );
 }
